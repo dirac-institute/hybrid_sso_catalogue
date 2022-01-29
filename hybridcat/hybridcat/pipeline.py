87aa396fde456799cb8596d11d7462b627e026cc
Tom Wagg <tomjwagg@gmail.com>
2022-01-28 22:00:18 -0800

    all preprocessing done

--- hybridcat/hybridcat/pipeline.py
+++ hybridcat/hybridcat/pipeline.py
@@ -6,18 +6,20 @@ from astropy.time import Time
 from time import time
 
 
-class Hybrid():
+class HybridCreator():
     def __init__(self, catalogue_folder="catalogues/", output_folder="output/",
                  d_max=0.1, n_neighbours=100, propagate_date="2022-03-01", recreate=False,
-                 save_all=True, save_final=True):
+                 dynmodel="2", save_all=True, save_final=True, verbose=False):
         self.catalogue_folder = catalogue_folder
         self.output_folder = output_folder
         self.d_max = d_max
         self.n_neighbours = n_neighbours
         self.propagate_date = propagate_date
         self.recreate = recreate
+        self.dynmodel = dynmodel
         self.save_all = save_all
         self.save_final = save_final
+        self.verbose = verbose
 
     def create_initial_catalogues(self):
         self.mpcorb = data.create_mpcorb_from_json(in_path=self.catalogue_folder + "mpcorb_extended.json",
@@ -28,36 +30,38 @@ class Hybrid():
                                               out_path=self.catalogue_folder + "s3m_initial.h5",
                                               recreate=self.recreate, save=self.save_all)
 
-    def transform_catalogues
+    def propagate_catalogues(self):
+        until_when = Time(self.propagate_date).mjd
+        self.mpcorb = transform_orbits.propagate_catalogues(self.mpcorb, until_when=until_when,
+                                                            dynmodel=self.dynmodel, initialise=True)
+        self.s3m = transform_orbits.propagate_catalogues(self.s3m, until_when=until_when,
+                                                         dynmodel=self.dynmodel, initialise=False)
+
+        if self.save_final:
+            self.mpcorb.to_hdf(self.catalogue_folder + "mpcorb_propagated.h5", key="df", mode="w")
+            self.s3m.to_hdf(self.catalogue_folder + "s3m_propagated.h5", key="df", mode="w")
 
     def preprocessing(self):
-        print("Started")
-        start = time()
+        if verbose:
+            print("Started preprocessing")
 
         self.create_initial_catalogues()
-        print("Catalogue created/read: {:1.2f}s".format(time() - start))
         
-        # mpcorb_com = transform_orbits.transform_catalogue(mpcorb, current_coords="KEP", transformed_coords="COM")
-        # mpcorb_com.to_hdf("../catalogues/cometary/mpcorb.h5", key="df", mode="w")
-        print("Coordinate conversion done: {:1.2f}s".format(time() - start))
+        if verbose:
+            print("Catalogues created/read")
         
-        march_first = Time("2022-03-01").mjd
-        # mpcorb_prop = transform_orbits.propagate_catalogues(mpcorb_com, until_when=march_first, initialise=False)
-        # mpcorb_prop.to_hdf("../catalogues/propagated/mpcorb.h5", key="df", mode="w")
-        s3m_prop = transform_orbits.propagate_catalogues(s3m, until_when=march_first, initialise=True)
-        s3m_prop.to_hdf("../catalogues/propagated/s3m.h5", key="df", mode="w")
+        self.mpcorb = transform_orbits.transform_catalogue(self.mpcorb, current_coords="KEP",
+                                                           transformed_coords="COM")
+        if self.save_all:
+            self.mpcorb.to_hdf(self.catalogue_folder + "mpcorb_cometary.h5", key="df", mode="w")
 
-        print("Orbits propagated")
-        print("Total Runtime: {:1.2f}s".format(time() - start))
+        if verbose:
+            print("mpcorb coordinate conversion done")
+        
+        self.propagate_catalogues()
+        if verbose:
+            print("Orbits propagated\nPreprocessing done")
 
-def main():
-    pass
-    # preprocessing()
-
-
-def test():
-    print("hey there tom")
-
-
-if __name__ == "__main__":
-    main()
+def merge_it():
+    the_creator = HybridCreator()
+    the_creator.preprocessing()
