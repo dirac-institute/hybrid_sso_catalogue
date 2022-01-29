8b339cd128448cb3051a3f36517ab462bdbf5548
Tom Wagg <tomjwagg@gmail.com>
2022-01-28 22:36:34 -0800

    get merging into a function

--- hybridcat/hybridcat/merge_catalogues.py
+++ hybridcat/hybridcat/merge_catalogues.py
@@ -1,123 +1,127 @@
 from scipy.spatial import cKDTree
 import numpy as np
 import pandas as pd
-from dask.distributed import Client, progress
 import dask
-
-
-def get_catalogues(s3m_path="catalogues/s3m_propagated.h5",
-                   mpcorb_path="catalogues/mpcorb_propagated.h5"):
-    
-    s3m = pd.read_hdf(s3m_path, key="df")
-    mpcorb = pd.read_hdf(mpcorb_path, key="df")
-    return s3m, mpcorb
+from dask.distributed import Client
 
 
 @dask.delayed
-def merge_catalogues(sim, real, min_mag, max_mag, k=100, d_max=0.1):
-    """Merge the simulated solar system catalogue with the real MPCORB data for a
-    certain magnitude bin.
-    
+def merge_magnitude_bin(sim, real, min_mag, max_mag, k=100, d_max=0.1, output_folder="output/"):
+    """Merge the simulated solar system catalogue with the real MPCORB data for a certain magnitude bin.
+
     Parameters
     ----------
-    
     sim : `pandas DataFrame`
         Dataframe with the simulated catalogue (must contain x,y,z,vx,vy,vz and H)
-    
     real : `pandas DataFrame`
         Dataframe with the real catalogue (must contain x,y,z,vx,vy,vz and H)
-        
     min_mag : `float`
         Minimum magnitude to consider for this merge
-        
     max_mag : `float`
         Maximum magnitude to consider for this merge
-        
-    k : `int`
-        Maximum number of neighbours to find
-        
-    d_max : `float`
-        Maxmimum distance within which to find neighbours
-        
+    k : `int`, optional
+        Maximum number of neighbours to find, by default 100
+    d_max : `float`, optional
+        Maximum distance within which to find neighbours in AU, by default 0.1
+    output_folder : `str`, optional
+        Path to the folder in which output will be stored, by default "output/"
+
     Returns
     -------
     taken_ids : `float/array`
-        An array of the ids that have been replaced by the real objects in this
-        magnitude bin
-        
-    no_match_count : `int`
-        A count of the number of systems that had no matches (and thus must be
-        added directly to the simulated catalogue)
+        An array of the ids that have been replaced by the real objects in this magnitude bin
     """
     real_xyz = np.array([real["x"].values, real["y"].values, real["z"].values]).T
     sim_xyz = np.array([sim["x"].values, sim["y"].values, sim["z"].values]).T
-    
+
     v_sim = np.array([sim.vx.values, sim.vy.values, sim.vz.values])
     v_real = np.array([real.vx.values, real.vy.values, real.vz.values]).T
-    
+
     # get the matching simulated data and build a K-D Tree
     sim_mag_mask = np.logical_and(sim.H >= min_mag, sim.H < max_mag)
     sim_id = sim.id[sim_mag_mask].values
     tree = cKDTree(sim_xyz[sim_mag_mask])
-    
+
     # get the matching real data from MPCORB
-#     print(min_mag, max_mag)
     real_mag_mask = np.logical_and(real.H >= min_mag, real.H <= max_mag)
     real_objects = real_xyz[real_mag_mask]
     real_velocities = v_real[real_mag_mask]
-    
-    # keep track of objects already assigned and a count of how many had no matches
+
+    # keep track of objects already assigned
     taken = []
-    no_match_count = 0
-    
-#     print(len(real_objects), len(real_velocities), len(real_xyz))
-    
+
     # iterate over every object in the real catalogue
     for obj, vel in zip(real_objects, real_velocities):
-        
+
         # find the nearest k neighbours within d_max and mask further neighbours
         distances, inds = tree.query(obj, k=k, distance_upper_bound=d_max)
         distances, inds = distances[np.isfinite(distances)], inds[np.isfinite(distances)]
-        
+
         # get only the options which haven't yet been assigned
         unassigned_inds = np.setdiff1d(inds, taken, assume_unique=True)
 
-#         print(obj, unassigned_inds)
-        
         # if there are many matching object
         if len(unassigned_inds) > 0:
             # find the closest velocity of the bunch and assign it
             best = np.sum((v_sim[:, unassigned_inds] - vel[:, np.newaxis])**2, axis=0).argmin()
             taken.append(unassigned_inds[best])
-        
+
         # if only one then just immediately assign it
         elif len(unassigned_inds) == 1:
             taken.append(unassigned_inds[0])
-        
-        # otherwise then there was no match
-        else:
-            no_match_count += 1
-            
-    np.save("output/matched_{}_{}.npy".format(min_mag, max_mag), np.array(sim_id[taken]))
+
+    np.save(output_folder + "matched_{}_{}.npy".format(min_mag, max_mag), np.array(sim_id[taken]))
             
     return np.array(sim_id[taken])
 
 
-if __name__ == "__main__":
-    
-    # start the Dask client
-    client = Client(n_workers=48, threads_per_worker=1, memory_limit='16GB')
+def merge_catalogues(mpcorb, s3m, output_folder="output/", H_bins=np.arange(-2, 28 + 1), n_workers=48,
+                     memory_limit="16GB", timeout=300, k=100, d_max=0.1):
+    """Merge mpcorb and s3m!
 
-    # get the catalogues
-    s3m, mpcorb = get_catalogues()
+    Parameters
+    ----------
+    mpcorb : `Pandas Dataframe`
+        MPCORB catalogue
+    s3m : `Pandas DataFrame`
+        S3m catalogue
+    output_folder : `str`, optional
+        Path to the output folder, by default "output/"
+    H_bins : `list`, optional
+        Magnitude bins, by default np.arange(-2, 28 + 1)
+    n_workers : `int`, optional
+        How many workers to use, by default 48
+    memory_limit : `str`, optional
+        Limit on the memory used by each worker, by default "16GB"
+    timeout : `int`, optional
+        Timeout for dask, by default 300
+    k : `int`, optional
+        Number of neighbours to consider in matching, by default 100
+    d_max : `float`, optional
+        Maximum distance a neighbour can occur at in AU, by default 0.1
+
+    Returns
+    -------
+    matched_ids : `list`
+        The IDs of S3m objects that can be replaced by MPCORB ones
+    """
+    # start the Dask client
+    client = Client(n_workers=n_workers, threads_per_worker=1, memory_limit=memory_limit)
+
+    # scatter the catalogues to avoid huge files
     s3m_handle, = client.scatter([s3m], broadcast=True)
     mpcorb_handle, = client.scatter([mpcorb], broadcast=True)
     
-    H_bins = np.arange(-2, 28 + 1)
+    # prepare the run each magnitude bin
     output = []
     for left, right in zip(H_bins[:-1], H_bins[1:]):
-        output.append(merge_catalogues(s3m_handle, mpcorb_handle, left, right))
+        output.append(merge_magnitude_bin(s3m=s3m_handle, mpcorb=mpcorb_handle, min_mag=left, max_mag=right,
+                                          k=k, d_max=d_max, output_folder=output_folder))
         
-    results = client.compute(output, timeout=300)
-    progress(*results)
-    np.save("output/all.npy", results)
+    # set them all running
+    results = client.compute(output, timeout=timeout)
+
+    # save the final result in 
+    np.save(output_folder + "all.npy", results)
+
+    return results
