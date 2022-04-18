4aa836c607fe26b13e95c3807d57a31cbf761882
Tom Wagg <twagg@college.harvard.edu>
2022-04-18 12:00:07 -0700

    linting

--- neocp/lsst_neocp.py
+++ neocp/lsst_neocp.py
@@ -33,16 +33,16 @@ plt.rcParams.update(params)
 
 
 def plot_neo_scores(neo_scores, density=False, bins=np.linspace(0, 100, 30), night=0):
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     handles = []
-    
+
     for score in neo_scores:
         ax.hist(score["scores"], bins=bins, facecolor=score["colour"][:-1] + [0.2],
                 label=score["label"], lw=3, density=density)
         ax.hist(score["scores"], bins=bins, color=score["colour"], histtype="step",
                 lw=3, density=density)
-        
+
         handles.append(Patch(facecolor=score["colour"][:-1] + [0.2],
                              edgecolor=score["colour"],
                              label=score["label"], lw=3))
@@ -50,12 +50,12 @@ def plot_neo_scores(neo_scores, density=False, bins=np.linspace(0, 100, 30), nig
     ax.legend(handles=handles, loc="upper center", ncol=2, fontsize=0.8 * fs)
 
     ax.set_xlabel("NEO Score")
-    
+
     if density:
         ax.set_ylabel("Density")
     else:
         ax.set_ylabel("Number of objects")
-        
+
     ax.set_title(f"Night {night}", fontsize=fs)
 
     return fig, ax
@@ -68,11 +68,11 @@ def get_specific_neo_score(path, file_name):
         if not ignore_me:
             df = pd.read_fwf(path + file_name)
             return df["NEO"].values, df["Desig."].values
-        
+
     return None, None
 
 
-def get_neo_scores(path, night=None):    
+def get_neo_scores(path, night=None):
     if night is None:
         neo_scores = np.array([])
         ids = np.array([])
@@ -95,8 +95,8 @@ def print_time_delta(start, end, label):
 
 """ --- digest2 prep functions --- """
 
+
 def filter_tracklets(df, min_obs=2, min_arc=1, max_time=90):
-    
     init = SkyCoord(ra=df["AstRA(deg)"].iloc[0], dec=df["AstDec(deg)"].iloc[0], unit="deg")
     final = SkyCoord(ra=df["AstRA(deg)"].iloc[-1], dec=df["AstDec(deg)"].iloc[-1], unit="deg")
 
@@ -106,18 +106,18 @@ def filter_tracklets(df, min_obs=2, min_arc=1, max_time=90):
 
 
 f2n = [[0, 1, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
- [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
- [31, 35, 36, 37, 39, 40, 41, 42, 43, 44, 45, 46],
- [46, 47, 49, 50, 52, 53, 54, 56, 57, 58, 59, 60, 61],
- [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72],
- [72, 73, 74, 75, 76, 77, 78, 79, 82, 83, 85, 86, 87, 88, 89],
- [89, 90, 91, 93, 94, 95, 98, 107, 108, 109, 110, 111, 112],
- [112, 113, 114, 115, 118, 119, 121, 122, 124, 128, 129, 130, 131, 132],
- [132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144],
- [144, 145, 146, 147, 148, 149, 150, 151, 152, 154, 157, 159],
- [159, 160, 161, 162, 163, 165, 166, 167, 168, 169, 170, 171],
- [171, 172, 173, 174, 175, 178, 179, 180, 181, 182, 183, 184, 185],
- [185, 186, 187, 188, 191, 192, 193, 196, 197, 198, 199, 200, 201, 202]]
+       [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+       [31, 35, 36, 37, 39, 40, 41, 42, 43, 44, 45, 46],
+       [46, 47, 49, 50, 52, 53, 54, 56, 57, 58, 59, 60, 61],
+       [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72],
+       [72, 73, 74, 75, 76, 77, 78, 79, 82, 83, 85, 86, 87, 88, 89],
+       [89, 90, 91, 93, 94, 95, 98, 107, 108, 109, 110, 111, 112],
+       [112, 113, 114, 115, 118, 119, 121, 122, 124, 128, 129, 130, 131, 132],
+       [132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144],
+       [144, 145, 146, 147, 148, 149, 150, 151, 152, 154, 157, 159],
+       [159, 160, 161, 162, 163, 165, 166, 167, 168, 169, 170, 171],
+       [171, 172, 173, 174, 175, 178, 179, 180, 181, 182, 183, 184, 185],
+       [185, 186, 187, 188, 191, 192, 193, 196, 197, 198, 199, 200, 201, 202]]
 
 
 def find_first_file(night_range):
@@ -130,20 +130,20 @@ def find_first_file(night_range):
 def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detections/march_start_v2.1/S0/",
                          out_path="neo/", night_zero=59638, start_night=0, final_night=31, timeit=False,
                          min_obs=2, min_arc=1, max_time=90, s3m_path="../catalogues/s3m_initial.h5"):
-    
+
     if timeit:
         start = time.time()
-    
+
     # convert all S3M IDs to hex so they fit
     s3m = pd.read_hdf(s3m_path)
     hex_ids = np.array([f'{num:07X}' for num in np.arange(len(s3m.index.values))])
     s3m_to_hex7 = dict(zip(s3m.index.values, hex_ids))
     print("done creating S3M ID mapping")
-    
+
     if timeit:
         print_time_delta(start, time.time(), label="S3M Mapping Creation")
         start = time.time()
-        
+
     night, file = start_night, find_first_file(range(start_night, final_night))
     files = sorted(listdir(in_path[0])) if isinstance(in_path, list) else sorted(listdir(in_path))
     columns_to_keep = ["ObjID", "FieldMJD", "AstRA(deg)", "AstDec(deg)", "filter", "MaginFilter"]
@@ -174,7 +174,7 @@ def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detec
                 df["night"] = (df["FieldMJD"] - 0.5).astype(int)
                 df["night"] -= night_zero
                 next_file = False
-                
+
                 if timeit:
                     print_time_delta(start, time.time(), label=f"Reading file {file}")
                     start = time.time()
@@ -185,10 +185,10 @@ def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detec
 
                 # sort by the object and then the time
                 df = df.sort_values(["ObjID", "FieldMJD"])
-                
+
                 # write the new file back out
                 df.to_hdf(out_path + f"filtered_visit_{file:03d}.h5", key="df")
-            
+
             if timeit:
                 print_time_delta(start, time.time(), label=f"Filtered file {file} done")
                 start = time.time()
@@ -206,19 +206,19 @@ def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detec
             # each line stars with 5 spaces
             lines = [" " * 5 for i in range(len(nightly_obs))]
             for i in range(len(nightly_obs)):
-                # squish the ID into the available space (TODO: this may be problematic)
+                # convert ID to its hex representation
                 lines[i] += s3m_to_hex7[nightly_obs.iloc[i]["ObjID"]]
-                
+
                 # add two spaces and a C (the C is important for some reason)
                 lines[i] += " " * 2 + "C"
 
                 # convert time to HH MM DD.ddddd format
                 t = datetimes[i]
                 lines[i] += "{:4.0f} {:02.0f} {:08.5f} ".format(t.year, t.month, t.day + nightly_obs.iloc[i]["FieldMJD"] % 1.0)
-                
+
                 # convert RA to HH MM SS.ddd
                 lines[i] += "{:02.0f} {:02.0f} {:06.3f}".format(ra_degrees.h[i], ra_degrees.m[i], ra_degrees.s[i])
-                
+
                 # convert Dec to sHH MM SS.dd
                 lines[i] += "{:+03.0f} {:02.0f} {:05.2f}".format(dec_degrees.h[i], abs(dec_degrees.m[i]), abs(dec_degrees.s[i]))
 
@@ -235,7 +235,7 @@ def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detec
             with open(out_path + "night_{:02d}.obs".format(night), "a" if append else "w") as obs_file:
                 obs_file.writelines(lines)
             append = False
-        
+
         if timeit:
             print_time_delta(start, time.time(), label=f"Writing observations for night {night}")
             start = time.time()
@@ -244,31 +244,32 @@ def create_digest2_input(in_path="/data/epyc/projects/jpl_survey_sim/10yrs/detec
         if night >= df["night"].max():
             file += 1
             next_file = True
-            
+
             # append obs next time
             append = True
         # if the file is still going then move to the next night
         else:
             print(str(len(nightly_obs)) + " observations in night " + str(night))
             night += 1
-            
-            
-def create_bash_script(out_path="neo/", start_night=0, final_night=31, digest2_path="/data/epyc/projects/hybrid-sso-catalogs/digest2/", cpu_count=32):
+
+
+def create_bash_script(out_path="neo/", start_night=0, final_night=31,
+                       digest2_path="/data/epyc/projects/hybrid-sso-catalogs/digest2/", cpu_count=32):
     bash = "for NIGHT in " + " ".join(["{:02d}".format(i) for i in range(start_night, final_night)]) + "\n"
     bash += "do\n"
 
-    bash += 'echo "Now running night $NIGHT through digest2..."\n'
-    bash += "\t" + f"time {digest2_path}digest2 -p {digest2_path} -c {digest2_path}MPC.config --cpu {cpu_count}"
-    bash += " {}night_$NIGHT.obs > {}night_$NIGHT.dat".format(out_path, out_path) + "\n"
-    bash += "grep -a -v tracklet {}night_$NIGHT.dat > {}night_$NIGHT.filtered.dat \n".format(out_path, out_path)
+    bash += 'echo "Now running night $NIGHT through digest2..."\n\t'
+    bash += f"time {digest2_path}digest2 -p {digest2_path} -c {digest2_path}MPC.config --cpu {cpu_count}"
+    bash += f" {out_path}night_$NIGHT.obs > {out_path}night_$NIGHT.dat" + "\n"
+    bash += f"grep -a -v tracklet {out_path}night_$NIGHT.dat > {out_path}night_$NIGHT.filtered.dat \n"
 
     bash += "done\n"
-    
+
     return bash
 
-        
+
 def main():
-    
+
     parser = argparse.ArgumentParser(description='Run digest2 on LSST mock observations')
     parser.add_argument('-i', '--in-path',
                         default="/data/epyc/projects/jpl_survey_sim/10yrs/detections/march_start_v2.1/S0/",
@@ -300,24 +301,26 @@ def main():
     parser.add_argument('-t', '--timeit', action="store_true",
                         help="Whether to time the code and print it out")
     args = parser.parse_args()
-    
+
     if args.mba:
         args.in_path = ["/data/epyc/projects/jpl_survey_sim/10yrs/detections/march_start_v2.1/S1_{:02d}/".format(i) for i in range(14)]
         args.out_path = "mba/"
-    
+
     if args.mba_hyak:
         args.in_path = [f'/gscratch/dirac/tomwagg/simulated_obs/S1_{i:02d}/' for i in range(14)]
         args.out_path = "/gscratch/dirac/tomwagg/hybrid_sso_catalogue/neocp/mba"
-        
+
     print("Starting digest2 run for", args.out_path)
 
     create_digest2_input(in_path=args.in_path, out_path=args.out_path, timeit=args.timeit,
-                         night_zero=args.night_zero, start_night=args.start_night, final_night=args.final_night,
-                         min_obs=args.min_obs, min_arc=args.min_arc, max_time=args.max_time, s3m_path=args.s3m_path)
-    
-    script = create_bash_script(out_path=args.out_path, start_night=args.start_night, final_night=args.final_night,
-                                digest2_path=args.digest2_path, cpu_count=args.cpu_count)
-    
+                         night_zero=args.night_zero, start_night=args.start_night,
+                         final_night=args.final_night, min_obs=args.min_obs, min_arc=args.min_arc,
+                         max_time=args.max_time, s3m_path=args.s3m_path)
+
+    script = create_bash_script(out_path=args.out_path, start_night=args.start_night,
+                                final_night=args.final_night, digest2_path=args.digest2_path,
+                                cpu_count=args.cpu_count)
+
     start = time.time()
     subprocess.call(script, shell=True)
     print_time_delta(start, time.time(), "total digest2 run")
